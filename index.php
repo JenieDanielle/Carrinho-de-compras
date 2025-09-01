a7f031016e133c68dbd8c1e32d57d0ec69f2dc8b
Jenie <jdft1101@gmail.com>
2025-09-01 10:25:21 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once "shoppingCart.php";
+
+$loja = new CartGenerator();
+$loja->addToCart(1, 2); // adiciona item
+$loja->addToCart(2, 6); // tenta adicionar mais do que o estoque da calça jeans
+$loja->addToCart(3, 2); // adiciona normalmente o tênis
+
+print_r($loja->getCart());
+
+//tirar esse dps
+print_r($loja->getProducts());
---
+++ shoppingCart.php
@@ -0,0 +1,99 @@
+<?php
+
+class cartGenerator
+{
+    private array $products;
+    private array $cart;
+
+    public function __construct()
+    {
+        $this->products = [
+            ['id' => 1, 'name' => 'Camiseta', 'price' => 59.90, 'stock' => 10],
+            ['id' => 2, 'name' => 'Calça Jeans', 'price' => 129.90, 'stock' => 5], 
+            ['id' => 3, 'name' => 'Tênis', 'price' => 199.90, 'stock' => 3]
+        ];
+
+        $this->cart = [];
+    }
+
+    public function findProduct($id)
+    {
+        foreach ($this->products as $key=>$product)
+        {
+            if ($product['id'] == $id) 
+            {
+                return $key;
+            }
+        }
+        
+        return null;
+    }
+
+    public function addToCart($productId, $quantity)
+    {
+        $key = $this->findProduct($productId);
+
+        if ($key !== null)
+        {
+            $product = $this->products[$key];
+
+            if ($product['stock'] >= $quantity)
+            {
+                $subtotal = $product['price'] * $quantity;
+                $this->cart[] = ['id' => $productId, 'name' => $product['name'], 'quantity' => $quantity, 'subtotal' => $subtotal];
+                $this->products[$key]['stock'] -= $quantity;
+            } else 
+            {
+                echo "Produto {$product['name']} não adicionado. O estoque é de {$product['stock']} unidades. \n";
+            }
+        } else 
+        {
+            echo "Produto não encontrado";
+        }
+    }
+
+    public function sumTotal()
+    {
+        $total = 0;
+
+        if (empty($this->cart))
+        {
+            echo "O carrinho está vazio";
+            return 0;
+        }
+
+        foreach ($this->cart as $product)
+        {
+            $total += $product['subtotal'];
+        }
+
+        return $total;
+    }
+
+
+    public function getCart() 
+    {
+        if (!empty($cart))
+        {
+            echo "<p>O carrinho de compras está vazio... \n</p>";
+        } else
+        {
+            echo "<p><b>Carrinho de Compras: </b></p>";
+                foreach ($this->cart as $product)
+                {
+                    echo "<p>- {$product['quantity']} {$product['name']} <br>R$ {$product['subtotal']}</p>";
+                }
+                echo "<p><b>Total:</b> R$ {$this->sumTotal()}</p>";
+        }
+
+        return $this->cart;
+    }
+
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+}
+
+
