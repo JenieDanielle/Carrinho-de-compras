9b0559780a0139bd6a109d5322ce80e2e2ccb4a4
Simone Siqueira de Melo <Sisiqueirakook1997@gmail.com>
2025-09-02 16:58:57 -0300

    Criação da função removeProducts

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ $loja = new CartGenerator();
 $loja->addToCart(1, 2); // adiciona item
 $loja->addToCart(2, 6); // tenta adicionar mais do que o estoque da calça jeans
 $loja->addToCart(3, 2); // adiciona normalmente o tênis
+$loja->removeProducts(3); // remove o tênis do carrinho
 
 print_r($loja->getCart());
 
 //tirar esse dps
 print_r($loja->getProducts());
+
--- shoppingCart.php
+++ shoppingCart.php
@@ -9,7 +9,7 @@ class cartGenerator
     {
         $this->products = [
             ['id' => 1, 'name' => 'Camiseta', 'price' => 59.90, 'stock' => 10],
-            ['id' => 2, 'name' => 'Calça Jeans', 'price' => 129.90, 'stock' => 5], 
+            ['id' => 2, 'name' => 'Calça Jeans', 'price' => 129.90, 'stock' => 5],
             ['id' => 3, 'name' => 'Tênis', 'price' => 199.90, 'stock' => 3]
         ];
 
@@ -18,14 +18,12 @@ class cartGenerator
 
     public function findProduct($id)
     {
-        foreach ($this->products as $key=>$product)
-        {
-            if ($product['id'] == $id) 
-            {
+        foreach ($this->products as $key => $product) {
+            if ($product['id'] == $id) {
                 return $key;
             }
         }
-        
+
         return null;
     }
 
@@ -33,21 +31,17 @@ class cartGenerator
     {
         $key = $this->findProduct($productId);
 
-        if ($key !== null)
-        {
+        if ($key !== null) {
             $product = $this->products[$key];
 
-            if ($product['stock'] >= $quantity)
-            {
+            if ($product['stock'] >= $quantity) {
                 $subtotal = $product['price'] * $quantity;
                 $this->cart[] = ['id' => $productId, 'name' => $product['name'], 'quantity' => $quantity, 'subtotal' => $subtotal];
                 $this->products[$key]['stock'] -= $quantity;
-            } else 
-            {
+            } else {
                 echo "Produto {$product['name']} não adicionado. O estoque é de {$product['stock']} unidades. \n";
             }
-        } else 
-        {
+        } else {
             echo "Produto não encontrado";
         }
     }
@@ -56,14 +50,12 @@ class cartGenerator
     {
         $total = 0;
 
-        if (empty($this->cart))
-        {
+        if (empty($this->cart)) {
             echo "O carrinho está vazio";
             return 0;
         }
 
-        foreach ($this->cart as $product)
-        {
+        foreach ($this->cart as $product) {
             $total += $product['subtotal'];
         }
 
@@ -71,19 +63,16 @@ class cartGenerator
     }
 
 
-    public function getCart() 
+    public function getCart()
     {
-        if (!empty($cart))
-        {
+        if (!empty($cart)) {
             echo "<p>O carrinho de compras está vazio... \n</p>";
-        } else
-        {
+        } else {
             echo "<p><b>Carrinho de Compras: </b></p>";
-                foreach ($this->cart as $product)
-                {
-                    echo "<p>- {$product['quantity']} {$product['name']} <br>R$ {$product['subtotal']}</p>";
-                }
-                echo "<p><b>Total:</b> R$ {$this->sumTotal()}</p>";
+            foreach ($this->cart as $product) {
+                echo "<p>- {$product['quantity']} {$product['name']} <br>R$ {$product['subtotal']}</p>";
+            }
+            echo "<p><b>Total:</b> R$ {$this->sumTotal()}</p>";
         }
 
         return $this->cart;
@@ -94,6 +83,24 @@ class cartGenerator
         return $this->products;
     }
 
+    public function removeProducts($productId)
+    {
+        foreach ($this->cart as $cartKey => $cartItem) 
+        {
+            $Key = $this->findProduct($productId);
+
+            if ($Key !== null) 
+            {
+                if ($cartItem['id'] == $productId) 
+                {
+                    unset($this->cart[$cartKey]);
+                    $this->cart = array_values($this->cart);
+                    $this->products[$Key]['stock'] += $cartItem['quantity'];
+
+                    echo "Produto {$cartItem['name']} removido do carrinho e devolvido ao estoque."; 
+                }
+            }
+            echo "Produto não existe dentro do carrinho.";
+        }
+    }
 }
-
-
